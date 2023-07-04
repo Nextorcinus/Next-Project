eeb203b7240e10469a7c55bffdab5f04fbec09c9
Ryouta <nextruangkerja@gmail.com>
2023-07-05 01:23:28 +0700

    add fix script

--- background-drop-animation/script.js
+++ background-drop-animation/script.js
@@ -1,11 +1,11 @@
-const body = document.body,
-  jsScroll = document.getElementsByClassName("js-scroll")[0],
-  height = jsScroll.getBoundingClientRect().height - 1,
-  speed = 0.04;
+const jsBody = document.querySelector(".jsbody");
+const jsScroll = document.querySelector(".js-scroll");
+const height = jsScroll.getBoundingClientRect().height - 1;
+const speed = 0.04;
 
 var offset = 0;
 
-body.style.height = Math.floor(height) + "px";
+jsBody.style.height = Math.floor(height) + "px";
 
 function smoothScroll() {
   offset += (window.pageYOffset - offset) * speed;
@@ -13,6 +13,7 @@ function smoothScroll() {
   var scroll = "translateY(-" + offset + "px) translateZ(0)";
   jsScroll.style.transform = scroll;
 
-  raf = requestAnimationFrame(smoothScroll);
+  requestAnimationFrame(smoothScroll);
 }
+
 smoothScroll();
