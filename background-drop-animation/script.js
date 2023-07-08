f1582f6346004e291432783f2a0f3def81e8622c
Ryouta <nextruangkerja@gmail.com>
2023-07-09 03:21:03 +0700

    add gsap script

--- background-drop-animation/script.js
+++ background-drop-animation/script.js
@@ -1,19 +1,52 @@
-const jsBody = document.querySelector(".jsbody");
-const jsScroll = document.querySelector(".js-scroll");
-const height = jsScroll.getBoundingClientRect().height - 1;
-const speed = 0.04;
+gsap.registerPlugin(ScrollTrigger);
 
-var offset = 0;
+gsap.from(".box-2", {
+  y: 650,
+  scrollTrigger: {
+    trigger: document.querySelector(".section"),
+    start: "top 50%",
+    end: "right 70%",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
 
-jsBody.style.height = Math.floor(height) + "px";
+gsap.to(".box-1", {
+  y: 560,
+  scrollTrigger: {
+    trigger: document.querySelector(".section"),
+    start: "top 50%",
+    end: "center 90%",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
 
-function smoothScroll() {
-  offset += (window.pageYOffset - offset) * speed;
+console.clear();
 
-  var scroll = "translateY(-" + offset + "px) translateZ(0)";
-  jsScroll.style.transform = scroll;
+const items = gsap.utils.toArray(".item");
+let currentItem = null;
 
-  requestAnimationFrame(smoothScroll);
-}
+items.forEach((e, i) => {
+  const content = e.querySelector(".content");
+  const t = gsap.to(content, {
+    height: "auto",
+    paused: true,
+  });
 
-smoothScroll();
+  e._accordionTween = t;
+
+  e.addEventListener("click", () => {
+    if (currentItem !== null) {
+      items[currentItem].classList.toggle("active");
+      if (currentItem === i) {
+        currentItem = null;
+        return t.reverse();
+      }
+      items[currentItem]._accordionTween.reverse();
+    }
+    e.classList.toggle("active");
+    t.play();
+    currentItem = i;
+  });
+});
