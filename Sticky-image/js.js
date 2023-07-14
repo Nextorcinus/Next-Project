e106d95e99599c8fb9d03215e86872266dd383f4
Ryouta <nextruangkerja@gmail.com>
2023-07-15 05:06:29 +0700

    add change

--- Sticky-image/js.js
+++ Sticky-image/js.js
@@ -1,29 +1,43 @@
-window.addEventListener("DOMContentLoaded", function () {
-  var bapaknyaList = document.querySelectorAll(".bapaknya");
+$(window)
+  .scroll(function () {
+    let o = $(window),
+      i = $("body"),
+      t = $(".section"),
+      s = o.scrollTop() + o.height() / 3;
+    t.each(function () {
+      if (
+        $(this).position().top <= s &&
+        $(this).position().top + $(this).height() > s
+      ) {
+        i.removeClass(function (o, i) {
+          return i.replace(/(^|\s)color-\s+/g, "").trim();
+        });
+        $(".section").removeClass("active");
+        $(this).addClass("active");
+      }
+    });
+  })
+  .scroll();
 
-  for (var i = 0; i < bapaknyaList.length; i++) {
-    var bapaknya = bapaknyaList[i];
-    var container = bapaknya.querySelector(".container");
-    var image = bapaknya.querySelector(".image");
-    var text = bapaknya.querySelector(".text");
+// Mendapatkan elemen .section-img-inner dan footer
+const sectionImgInner = document.querySelector(".section-img-inner");
+const footer = document.querySelector("footer");
 
-    container.addEventListener("scroll", handleScroll(container, image, text));
+// Menghitung jarak antara elemen .section-img-inner dan footer
+const distance =
+  footer.offsetTop - sectionImgInner.offsetTop - sectionImgInner.offsetHeight;
+
+// Memantau perubahan posisi scroll
+window.addEventListener("scroll", function () {
+  // Mendapatkan posisi scroll vertikal
+  const scrollPosition = window.pageYOffset;
+
+  // Memeriksa apakah bagian bottom .section-img-inner bertemu dengan footer
+  if (scrollPosition > distance) {
+    // Mengubah posisi elemen .section-img-inner menjadi relative
+    sectionImgInner.style.position = "relative";
+  } else {
+    // Mengubah posisi elemen .section-img-inner menjadi fixed
+    sectionImgInner.style.position = "fixed";
   }
 });
-
-function handleScroll(container, image, text) {
-  return function () {
-    var containerTop = container.getBoundingClientRect().top;
-    var imageTop = image.getBoundingClientRect().top;
-    var textBottom = text.getBoundingClientRect().bottom;
-
-    if (imageTop <= containerTop) {
-      image.style.position = "fixed";
-      image.style.top = "0";
-      text.style.overflowY = "scroll";
-    } else if (imageTop >= textBottom) {
-      image.style.position = "static";
-      text.style.overflowY = "auto";
-    }
-  };
-}
