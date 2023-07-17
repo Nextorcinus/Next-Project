d6e8257ee674e26a57c128aa6982bd2658ddc6c8
Ryouta <nextruangkerja@gmail.com>
2023-07-17 08:04:44 +0700

    update js

--- Sticky-image/js.js
+++ Sticky-image/js.js
@@ -1,43 +1,43 @@
-$(window)
-  .scroll(function () {
-    let o = $(window),
-      i = $("body"),
-      t = $(".section"),
-      s = o.scrollTop() + o.height() / 3;
-    t.each(function () {
-      if (
-        $(this).position().top <= s &&
-        $(this).position().top + $(this).height() > s
-      ) {
-        i.removeClass(function (o, i) {
-          return i.replace(/(^|\s)color-\s+/g, "").trim();
-        });
+$(document).ready(function () {
+  $(window)
+    .scroll(function () {
+      let o = $(window),
+        i = $("body"),
+        t = $(".section"),
+        footer = $("footer"),
+        footerTop = footer.position().top;
+      let s = o.scrollTop() + o.height() / 3;
+      let container = $(".lay_3843_400");
+      let containerTop = container.offset().top;
+
+      t.each(function () {
+        if (
+          $(this).position().top <= s &&
+          $(this).position().top + $(this).height() > s
+        ) {
+          i.removeClass(function (o, i) {
+            return i.replace(/(^|\s)color-\s+/g, "").trim();
+          });
+          $(".section").removeClass("active");
+          $(this).addClass("active");
+
+          if (containerTop <= o.scrollTop()) {
+            $(".section-img-inner").css({
+              position: "fixed",
+              width: "40%",
+            });
+          } else {
+            $(".section-img-inner").css({
+              position: "relative",
+              width: "100%",
+            });
+          }
+        }
+      });
+
+      if (o.scrollTop() + o.height() >= footerTop) {
         $(".section").removeClass("active");
-        $(this).addClass("active");
       }
-    });
-  })
-  .scroll();
-
-// Mendapatkan elemen .section-img-inner dan footer
-const sectionImgInner = document.querySelector(".section-img-inner");
-const footer = document.querySelector("footer");
-
-// Menghitung jarak antara elemen .section-img-inner dan footer
-const distance =
-  footer.offsetTop - sectionImgInner.offsetTop - sectionImgInner.offsetHeight;
-
-// Memantau perubahan posisi scroll
-window.addEventListener("scroll", function () {
-  // Mendapatkan posisi scroll vertikal
-  const scrollPosition = window.pageYOffset;
-
-  // Memeriksa apakah bagian bottom .section-img-inner bertemu dengan footer
-  if (scrollPosition > distance) {
-    // Mengubah posisi elemen .section-img-inner menjadi relative
-    sectionImgInner.style.position = "relative";
-  } else {
-    // Mengubah posisi elemen .section-img-inner menjadi fixed
-    sectionImgInner.style.position = "fixed";
-  }
+    })
+    .scroll();
 });
