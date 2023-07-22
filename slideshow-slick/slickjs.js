a5f563226e6d34e147df4618464353947e794eac
Ryouta <nextruangkerja@gmail.com>
2023-07-22 14:18:54 +0700

    add slide sick js

---
+++ slideshow-slick/slickjs.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".carrousel").slick({
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    arrows: true,
+    autoplaySpeed: 2000,
+  });
+});
